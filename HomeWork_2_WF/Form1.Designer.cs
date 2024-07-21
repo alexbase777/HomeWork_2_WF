43c0eb23395445b45802bcc2f0bd3556dc05aab8
Алексей <tm23515417@gmail.com>
2024-07-21 18:07:08 +0500

    Add project files.

---
+++ HomeWork_2_WF/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace HomeWork_2_WF
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            tB1 = new TextBox();
+            btn1 = new Button();
+            btn2 = new Button();
+            openFileDialog1 = new OpenFileDialog();
+            menuStrip1 = new MenuStrip();
+            менюToolStripMenuItem = new ToolStripMenuItem();
+            загрузитьToolStripMenuItem = new ToolStripMenuItem();
+            закрытьToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tB1
+            // 
+            resources.ApplyResources(tB1, "tB1");
+            tB1.Name = "tB1";
+            // 
+            // btn1
+            // 
+            resources.ApplyResources(btn1, "btn1");
+            btn1.Name = "btn1";
+            btn1.UseVisualStyleBackColor = true;
+            btn1.Click += btn1_Click;
+            // 
+            // btn2
+            // 
+            resources.ApplyResources(btn2, "btn2");
+            btn2.Name = "btn2";
+            btn2.UseVisualStyleBackColor = true;
+            btn2.Click += btn2_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { менюToolStripMenuItem });
+            resources.ApplyResources(menuStrip1, "menuStrip1");
+            menuStrip1.Name = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            менюToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { загрузитьToolStripMenuItem, закрытьToolStripMenuItem });
+            менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            resources.ApplyResources(менюToolStripMenuItem, "менюToolStripMenuItem");
+            // 
+            // загрузитьToolStripMenuItem
+            // 
+            загрузитьToolStripMenuItem.Name = "загрузитьToolStripMenuItem";
+            resources.ApplyResources(загрузитьToolStripMenuItem, "загрузитьToolStripMenuItem");
+            загрузитьToolStripMenuItem.Click += btn1_Click;
+            // 
+            // закрытьToolStripMenuItem
+            // 
+            закрытьToolStripMenuItem.Name = "закрытьToolStripMenuItem";
+            resources.ApplyResources(закрытьToolStripMenuItem, "закрытьToolStripMenuItem");
+            закрытьToolStripMenuItem.Click += btn2_Click;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(btn2);
+            Controls.Add(btn1);
+            Controls.Add(tB1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox tB1;
+        private Button btn1;
+        private Button btn2;
+        private OpenFileDialog openFileDialog1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem менюToolStripMenuItem;
+        private ToolStripMenuItem загрузитьToolStripMenuItem;
+        private ToolStripMenuItem закрытьToolStripMenuItem;
+    }
+}
---
+++ HomeWork_2_WF/Form1.cs
@@ -0,0 +1,100 @@
+﻿
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Configuration;
+using System.Security.AccessControl;
+
+namespace HomeWork_2_WF
+{
+    public partial class Form1 : Form
+    {
+        string fileName_;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = Directory.GetCurrentDirectory(); // Путь к fb2-файлу
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) // Диалоговое окно
+            {
+                fileName_ = openFileDialog1.FileName; // Путь к файлу             
+
+                // --------------- Первый вариант вывода книги с тегами ----------------
+                //StreamReader sr = new StreamReader(fileName_);
+                /*string allText = sr.ReadToEnd();                
+                allText = allText.Replace("<p>", "");
+                allText = allText.Replace("</p>", "");                
+                tB1.Text = allText;
+                sr.Close();*/
+
+                // ------- Второй вариант вывода книги сплошным текстом-кодом без тегов ----------                                
+                /*XElement el = XElement.Load(fileName_);
+                tB1.Text = el.Value;*/
+
+                // ------------ Третий вариант вывода книги сплошным текстом без тегов и кода -----
+                /*XDocument doc = XDocument.Load(fileName_);
+                var namespaceManager = new XmlNamespaceManager(new NameTable());
+                namespaceManager.AddNamespace("fb", "http://www.gribuser.ru/xml/fictionbook/2.0");
+                var body = doc.Root.XPathSelectElements("fb:body", namespaceManager).ToList();
+                foreach (var item in body)
+                {
+                    tB1.Text = item.Value.ToString();
+                }*/
+
+                // ------------ Четвёртый вариант вывода книги с форматированием -----------
+                XDocument doc = XDocument.Load(fileName_); // Загружаем книгу fb2 в объект XDocument
+                XNamespace fb2NS = "http://www.gribuser.ru/xml/fictionbook/2.0"; // Элемент body у fb2 храниться в неймспейсе, который надо прописать
+                // Вывод анотации к книге
+                var annotation = doc.Root.Element(fb2NS + "description").Element(fb2NS + "title-info").Element(fb2NS + "annotation");
+                if (annotation != null)
+                    tB1.Text += "АННОТАЦИЯ:" + Environment.NewLine + Environment.NewLine + annotation.Value.ToString() + Environment.NewLine + Environment.NewLine;
+                var body = doc.Root.Element(fb2NS + "body"); // Выбираем элемент body
+                var title_book = body.Element(fb2NS + "title"); // В элементе body находим название книги
+                var paragraphs_title_book = title_book.Elements(fb2NS + "p"); // Разбиваем название книги по абзацам
+                if (paragraphs_title_book != null) // Выводим название книги в textbox
+                {
+                    foreach (var paragraph in paragraphs_title_book)
+                        tB1.Text += "\t\t\t" + paragraph.Value.ToString().ToUpper() + Environment.NewLine;
+                    tB1.Text += Environment.NewLine;
+                }
+                var sections = body.Elements(fb2NS + "section"); // Разбиваем тело книги по главам (тег section)                                
+                foreach (var section in sections)
+                {
+                    var title = section.Element(fb2NS + "title"); // В каждой главе находим заголовок
+                    if (title == null) // Если мы нашли не главу
+                    {
+                        var paragraphs = section.Elements(fb2NS + "p");
+                        if (paragraphs != null)
+                            foreach (var paragraph in paragraphs)
+                                tB1.Text += paragraph.Value.ToString() + Environment.NewLine;
+                        tB1.Text += Environment.NewLine;
+                    }
+                    if (title != null) // Если мы нашли главу с заголовком
+                    {
+                        var paragraphs_title = title.Elements(fb2NS + "p"); // В каждой главе находим абзатцы
+                        if (paragraphs_title != null)  // Выводим в textbox заголовок главы + её содержимое
+                        {
+                            foreach (var paragraph in paragraphs_title) // Вывод названия главы
+                                tB1.Text += "\t\t\t" + paragraph.Value.ToString().ToUpper() + Environment.NewLine;
+                            tB1.Text += Environment.NewLine;
+                            var paragraphs_section = section.Elements(fb2NS + "p");
+                            foreach (var paragraph in paragraphs_section) // Вывод содержимого глава
+                                tB1.Text += paragraph.Value.ToString() + Environment.NewLine + Environment.NewLine;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
